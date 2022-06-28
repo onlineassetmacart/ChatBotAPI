f567977b49866e8258318f95c7bf99125ddb1ea9
thanachote <thanachote@efinancethai.com>
2022-06-28 12:06:27 +0700

    init project

---
+++ Controllers/Chat2Controller.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatBotCenterAPI.Controllers
+{
+    public class Chat2Controller : Controller
+    {
+        //public Chat2Controller(IConfiguration _config, IHttpContextAccessor _context)
+        //{
+        //    string xx = "";
+        //}
+
+        public class ClsParamGetListChat
+        {
+            public string user_id { get; set; } = "";
+        }
+
+        [HttpPost("[controller]/GetListChat")]
+        public async Task<IActionResult> GetListChat([FromBody] ClsParamGetListChat param)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return Ok("hello");
+        }
+    }
+}

9077ec40cb5a931dc759afe57f777766e4681fd1
thanachote <thanachote@efinancethai.com>
2022-06-29 12:06:07 +0700

    เพิ่มการเรียกค่าหุ้น เชื่อมต่อกับ api พี่วัลย์

--- Controllers/Chat2Controller.cs
+++ Controllers/Chat2Controller.cs
@@ -5,6 +5,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+using System.Xml;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace ChatBotCenterAPI.Controllers
 {
@@ -15,23 +20,52 @@ namespace ChatBotCenterAPI.Controllers
         //    string xx = "";
         //}
 
+        private static readonly HttpClient client = new HttpClient();
+
         public class ClsParamGetListChat
         {
-            public string user_id { get; set; } = "";
+            public string symbol_name { get; set; } = "";
         }
 
         [HttpPost("[controller]/GetListChat")]
         public async Task<IActionResult> GetListChat([FromBody] ClsParamGetListChat param)
         {
+            var responseString = "";
+
             try
             {
+                var values = new Dictionary<string, string>
+                  {
+                      { "key", "@eft" },
+                      { "cmd", "005" },
+                      { "param", param.symbol_name }
+                  };
+
+                var content = new FormUrlEncodedContent(values);
+
+                var response = await client.PostAsync("https://demo.efinancethai.com/smartWS/service.asmx/ReturnValue", content);
+
+                var strRet = await response.Content.ReadAsStringAsync();
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(strRet);
+                strRet = doc.InnerText;
+
+                if (strRet != "") {
+                    var arrStr = strRet.Split(";");
+                    Dictionary<string, string> Dict = new Dictionary<string, string>();
+                    Dict.Add("symbol_name", arrStr[0]);
+                    Dict.Add("value", arrStr[1]);
+
+                    responseString = JsonConvert.SerializeObject(Dict);
+                }
+                
 
             }
             catch (Exception ex)
             {
 
             }
-            return Ok("hello");
+            return Ok(responseString);
         }
     }
 }
